b012e9382c4a7716f2a84ab49b18519fbfec4dab
alxdenisov <denalexsr727@gmail.com>
2025-06-26 22:12:34 +0300

    KS-7-task-1

---
+++ src/main/kotlin/KS_7_task_1.kt
@@ -0,0 +1,9 @@
+
+fun main(){
+    val chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#\$%^&*"
+    val randomString = (1..8)
+        .map { chars.random() }
+        .joinToString("")
+
+    println(randomString)
+}
